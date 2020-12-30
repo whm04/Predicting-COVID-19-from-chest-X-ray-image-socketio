605ac2ae359788e0d79d2c7fd513cf1081dd83c8
wahib <46822616+whm04@users.noreply.github.com>
2020-12-30 23:25:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,23 @@
 
 from flask_socketio import SocketIO
 from flask_socketio import send, emit
+from flask import Flask, render_template, request
+
 
 import cv2
 import numpy as np
 from PIL import Image
 
 import re
-
-from io import StringIO
 from io import BytesIO
 from PIL import Image
 import base64
 
-from flask import Flask, render_template, request
-from flask_ngrok import run_with_ngrok
-import os
-from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.models import load_model
+
 model = load_model('covid19.model')
 
 def predict(image):
-    
     image = cv2.resize(image, (224,224) )
     image =image.reshape((1, 224, 224,3))
     image =image /255.0
@@ -34,38 +30,36 @@ def predict(image):
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
-app.config['SECRET_KEY'] = '*************'
+app.config['SECRET_KEY'] = 'secret'
 socketio = SocketIO(app)
-
-
 
 
 @app.route('/', methods=['post', 'get'])
 def login():
-    
-    message = ''
         
     return render_template('final.html')
 
 
 
 @socketio.on('my event', namespace='/test')
-def handle_message(data):
-   
-
+def handle_message(data):   
     image_b64 = data['data']
     image_data = re.sub('^data:image/.+;base64,', '', image_b64)
     
-    
     im = Image.open(BytesIO(base64.b64decode(image_data)))
-
     im = im.convert("RGB")
     im=np.array(im)
-    print(im.shape)
+    
     positive,negative=predict(im)
     
     emit('log', dict(data=str(positive),d=str(negative)), broadcast=True)
     
 
+
+
+
+
+
     
-socketio.run(app,debug=True)
+socketio.run(app,debug=True )
+
